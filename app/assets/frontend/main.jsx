50a777c84da00e59debc1d68aad7a38d6d871199
Nick Pisciotta <npisciotta@gmail.com>
2016-10-13 16:00:39 -0600

    Install webpack and necessary modules Create TweetBox jsx file

---
+++ app/assets/frontend/main.jsx
@@ -0,0 +1,20 @@
+import TweetBox from "./components/TweetBox"
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <TweetBox />
+      </div>
+    );
+  }
+}
+
+let documentReady = () => {
+  ReactDOM.render(
+  <Main />,
+  document.getElementById('react')
+  );
+};
+
+$(documentReady);
