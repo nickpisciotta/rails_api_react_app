dd1115eaff2954937a3cea8edd14349831af78e0
Nick Pisciotta <npisciotta@gmail.com>
2016-10-14 12:20:30 -0600

    Add user first name and last and ability to update these attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'profile' => "profile#index"
-  post 'profile' => 'profile#update'
+  patch 'profile' => 'profile#update'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
