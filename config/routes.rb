e359f60eec27ef5fa8dc0b19700de0cd27a18f58
Nick Pisciotta <npisciotta@gmail.com>
2016-10-14 12:11:51 -0600

    Update profile page and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'profile/index'
+  get 'profile' => "profile#index"
+  post 'profile' => 'profile#update'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
