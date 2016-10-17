80570ae8267de6462cf56e99b98cb24f0baf2b8c
Nick Pisciotta <npisciotta@gmail.com>
2016-10-17 17:34:05 -0600

    Add followers resource and API endpoint route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  resources :followers
   resources :tweets
   get 'profile' => "profile#index"
   patch 'profile' => 'profile#update'
+
+  resources :followers do
+    collection do
+      get "random"
+    end
+  end
+
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
